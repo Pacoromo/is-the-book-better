943c2474fafa3eb243c7494a0d44c6a0c24ebe8d
pacoromo <pacoromo@me.com>
2023-02-16 10:48:41 -0500

    proxy update

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-
       <div className="wrapper">
         <div className="header-container">
           <Link to='/'>
